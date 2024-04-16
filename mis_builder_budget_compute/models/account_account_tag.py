d71a1b44fc542896e031f650a145e2eef5863e85
Henrik Norlin <henrik.norl@gmail.com>
2024-04-16 09:46:34 +0200

    [IMP] mis_builder_budget_compute: account tags with old and new kpi

--- mis_builder_budget_compute/models/account_account_tag.py
+++ mis_builder_budget_compute/models/account_account_tag.py
@@ -4,4 +4,27 @@ from odoo import _, api, fields, models
 class AccountAccountTag(models.Model):
     _inherit = "account.account.tag"
 
-    budget_percent = fields.Float()
+    budget_kpi_old = fields.Float()
+    budget_kpi_new = fields.Float(
+        compute='_get_budget_kpi_new',
+        readonly=False,
+        store=True,
+    )
+    budget_percent = fields.Float(
+        compute='_get_budget_percent',
+        readonly=False,
+        store=True,
+    )
+
+    @api.onchange("budget_kpi_old", "budget_percent")
+    def _get_budget_new_api(self):
+        for record in self:
+            if record.budget_kpi_old:
+                record.budget_kpi_new = record.budget_kpi_old * (1 + record.budget_percent)
+
+    @api.onchange("budget_kpi_old", "budget_kpi_new")
+    def _get_budget_percent(self):
+        for record in self:
+            if record.budget_kpi_old:
+                if record.budget_kpi_old and record.budget_kpi_new:
+                    record.budget_percent = record.budget_kpi_new / record.budget_kpi_old - 1
