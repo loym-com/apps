078af40bb5bfc8dc27923d8bcf6aaddab2477c5a
Henrik Norlin <henrik.norl@gmail.com>
2024-06-16 20:51:49 +0200

    [ADD] donation_analytic_description, dionation_report_analytic, donation_thanks

---
+++ donation_report_analytic/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Donation Analytic Report",
+    "summary": "",
+    "author": "Henrik Norlin, Odoo Community Association (OCA)",
+    "category": "Accounting & Finance",
+    "data": [
+        "views/donation_report_views.xml",
+    ],
+    "depends": [
+        "donation",
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["norlinhenrik"],
+    "version": "15.0.1.0.0",
+    "website": "https://github.com/OCA/donation",
+}
