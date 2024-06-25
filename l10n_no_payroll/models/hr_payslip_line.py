cd667f758df28b69f749716cc425fceee02680fc
Henrik Norlin <henrik.norl@gmail.com>
2024-06-25 12:59:03 +0200

    payroll: show inactive recurring lines, compute vacation money

--- l10n_no_payroll/models/hr_payslip_line.py
+++ l10n_no_payroll/models/hr_payslip_line.py
@@ -1,6 +1,6 @@
 import logging
 from collections import defaultdict
-from datetime import datetime
+from datetime import date, datetime
 
 from odoo import api, fields, models
 from odoo.exceptions import UserError
@@ -72,6 +72,7 @@ class HrPayslipLine(models.Model):
         # Get info from the payslip lines into dictinary 'd'
         d = defaultdict(dict)
         payslip_lines = self.search([])
+        # payslip_lines = self.search([("date_from", "<", date(2024, 1, 1))])
         for line in payslip_lines:
             employee_id = line.employee_id.id
             rule = line.salary_rule_id
