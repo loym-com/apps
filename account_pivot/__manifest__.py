a510b33c49ee5d8dfcedf47b73e79cb5fd2ffb5e
Henrik Norlin <henrik.norl@gmail.com>
2024-07-16 12:16:13 +0200

    [ADD] account_pivot

---
+++ account_pivot/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2019-2023 Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Account Pivot",
+    "summary": "",
+    "author": "Henrik Norlin, Odoo Community Association (OCA)",
+    "category": "account",
+    "data": [
+        "security/ir.model.access.csv",
+        "security/ir.rule.csv",
+        "views/account_account_views.xml",
+        "views/account_pivot_views.xml",
+    ],
+    "depends": [
+        "account",
+    ],
+    "license": "AGPL-3",
+    "maintainers": ["norlinhenrik"],
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA",
+}
