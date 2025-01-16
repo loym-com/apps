55bd27ad4bff0c684a1fca435af85cbddda1022b
Henrik Norlin <henrik.norl@gmail.com>
2025-01-16 12:32:02 +0100

    [ADD] hr_expense_analytic

---
+++ hr_expense_analytic/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2024 Henrik Norlin
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Hr Expense Analytic",
+    "summary": """
+        product >> analytic distribution""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Henrik Norlin,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/account-analytic",
+    "depends": [
+        "hr_expense",
+    ],
+}
---
+++ hr_expense_analytic/readme/DESCRIPTION.md
@@ -0,0 +1,1 @@
+This module updates hr.expense analytic_distribution from the product.
