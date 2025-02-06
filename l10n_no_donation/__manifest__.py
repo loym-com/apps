f0faaa01207e8f28d1938cc626a166bab1d3b24b
Henrik Norlin <henrik.norl@gmail.com>
2025-02-06 20:51:59 +0100

    [ADD] l10n_no_donation

---
+++ l10n_no_donation/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2019-2023 Ows - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Skattefradrag for gaver",
+    "summary": "",
+    "version": "16.0.1.0.0",
+    "author": "Henrik Norlin, Odoo Community Association (OCA)",
+    "category": "Localization",
+    "data": [
+        "security/ir.rule.csv",
+        "security/ir.model.access.csv",
+        "data/data.xml",
+        "views/views.xml",
+    ],
+    "depends": [
+        "donation",  # OCA/donation
+        "partner_identification",  # OCA/partner-contact
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["norlinhenrik"],
+    "website": "https://github.com/OCA",
+}
---
+++ l10n_no_donation/readme/USAGE.rst
@@ -0,0 +1,23 @@
+USAGE
+
+Company
+
+- name
+- vat
+- l10n_no_donation_partner_id
+
+Donation Contact Person
+
+- name
+- phone
+- mobile
+- email
+
+Donor Contacts
+
+- name
+- donation_total
+- id_numbers/category_id ("Fødsels-/D-nummer")
+- id_numbers/name (Fødsels-/D-nummer)
+
+Menu to export: Donation - Reporting - Norwegian Donation Report XML
