ab8d456716a3cb01a197313d54a3b476219a46a3
Henrik Norlin <henrik.norl@gmail.com>
2025-04-22 18:30:40 +0200

    fix donation_partner & donation_thanks (moved thanks_template_id)

--- donation_partner/models/donation_tax_receipt.py
+++ donation_partner/models/donation_tax_receipt.py
@@ -21,3 +21,9 @@ class DonationTaxReceipt(models.Model):
         string="Donor tags",
         help="Use tags on the donor to segment e.g. communication (email / snailmail)",
     )
+    thanks_template_id = fields.Many2one(
+        "donation.thanks.template",
+        string="Thanks Template",
+        ondelete="restrict",
+        copy=False,
+    )
