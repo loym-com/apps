3a7ddf29eb93e7c7ed1f5501134b4ef71a9d24ad
Henrik Norlin <henrik.norl@gmail.com>
2025-05-27 10:55:39 +0200

    donation_thanks: thanks odt is good (static image, dynamic text)

--- donation_thanks/models/donation_thanks_template.py
+++ donation_thanks/models/donation_thanks_template.py
@@ -8,6 +8,26 @@ _logger = logging.getLogger(__name__)
 class DonationThanksTemplate(models.Model):
     _inherit = "donation.thanks.template"
 
+    model = fields.Selection(
+        string="Model",
+        selection=[
+            ("donation.donation", "Donation"),
+            ("donation.thanks", "Donation Thanks"),
+            ("donation.tax.receipt", "Donation Tax Receipt"),
+        ]
+    )
+    attachment_ids = fields.One2many(
+        string="Attachments",
+        comodel_name="ir.attachment",
+        inverse_name="res_id",
+        context={"default_res_model": "donation.thanks.template"},
+    )
+    img_text = fields.Text(
+        string="Image Text",
+        translate=True,
+    )
+
+    # DEPRECATED
     image_height = fields.Integer("Image Height")
     image_width = fields.Integer("Image Width")
     image_text = fields.Html(
@@ -21,17 +41,4 @@ class DonationThanksTemplate(models.Model):
     text2 = fields.Html(
         sanitize=False, # Gives the best UI for translation
         translate=True,
-    )
-    attachment_ids = fields.One2many(
-        string="Attachments",
-        comodel_name="ir.attachment",
-        inverse_name="res_id",
-        context={"default_res_model": "donation.thanks.template"},
-    )
-    model = fields.Selection(
-        string="Model",
-        selection=[
-            ("donation.donation", "Donations"),
-            ("donation.tax.receipt", "Tax Receipts"),
-        ]
     )
