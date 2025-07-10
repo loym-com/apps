55fd608bbe974fc038c6886cfa070edc3aaec1ec
Henrik Norlin <henrik@loym.com>
2025-07-10 13:00:21 +0000

    fleet_vehicle_odometer: plan_id filter for analytic_account_id FIX

---
+++ fleet_vehicle_odometer/models/analytic_plan_mixin.py
@@ -0,0 +1,17 @@
+from odoo import _, api, fields, models
+
+
+class AnalyticPlanMixin(models.AbstractModel):
+    _name = "analytic.plan.mixin"
+    _description = "Analytic Plan Mixin"
+
+    plan_id = fields.Many2one("analytic.plan")
+    analytic_account_id = fields.Many2one("analytic.account")
+
+    @api.onchange("plan_id")
+    def _onchange_plan_id(self):
+        for rec in self:
+            if rec.plan_id:
+                return {"domain": {"analytic_account_id": [("plan_id", "=", rec.plan_id.id)]}}
+            else:
+                return {"domain": {"analytic_account_id": []}}
