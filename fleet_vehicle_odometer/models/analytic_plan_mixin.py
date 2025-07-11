1c0d79d77671970f6f678442cca03f51793d9099
Henrik Norlin <henrik@loym.com>
2025-07-11 07:33:50 +0000

    fleet_vehicle_odometer: analytic_plan_id test

--- fleet_vehicle_odometer/models/analytic_plan_mixin.py
+++ fleet_vehicle_odometer/models/analytic_plan_mixin.py
@@ -5,13 +5,33 @@ class AnalyticPlanMixin(models.AbstractModel):
     _name = "analytic.plan.mixin"
     _description = "Analytic Plan Mixin"
 
-    analytic_plan_id = fields.Many2one("account.analytic.plan")
+    analytic_plan_id = fields.Many2one(
+        comodel_name="account.analytic.plan",
+        # compute="_compute_analytic_plan_id",
+        inverse="_inverse_analytic_plan_id",
+        related="analytic_account_id.plan_id",
+        readonly=False,
+        store=False,
+        string="Analytic Plan",
+    )
     analytic_account_id = fields.Many2one("account.analytic.account")
 
     @api.onchange("analytic_plan_id")
     def _onchange_analytic_plan_id(self):
         for rec in self:
             if rec.analytic_plan_id:
-                return {"domain": {"analytic_account_id": [("plan_id", "=", rec.analytic_plan_id.id)]}}
+                rec.analytic_account_id = rec.analytic_plan_id.account_id
             else:
-                return {"domain": {"analytic_account_id": []}}
+                rec.analytic_account_id = False
+
+    def _inverse_analytic_plan_id(self):
+        for rec in self:
+            rec.analytic_plan_id = rec.analytic_plan_id or False
+
+    @api.onchange("analytic_account_id")
+    def _onchange_analytic_account_id(self):
+        for rec in self:
+            if rec.analytic_account_id:
+                rec.analytic_plan_id = rec.analytic_account_id.plan_id
+            else:
+                rec.analytic_plan_id = False
