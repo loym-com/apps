cc86be99b29c9f6cbfd997e56fe370070186b71f
Henrik Norlin <henrik.norl@gmail.com>
2025-07-18 16:56:33 +0200

    [ADD] knowledge_article_starred, knowledge_article_as_task

---
+++ knowledge_article_as_task/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2025 Loym
+
+{
+    "name": "Knowledge Article as Task",
+    "summary": "",
+    "author": "Loym",
+    "data": [
+        "data/ir_actions_act_window.xml",
+        "data/ir_actions_server.xml",
+        "data/ir_model_fields.xml",
+        "data/ir_ui_view.xml",
+    ],
+    "depends": [
+        "analytic",
+        "knowledge",
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "website": "https://www.loym.com",
+}
---
+++ knowledge_article_starred/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2025 Loym
+
+{
+    "name": "Filter on starred knowledge articles",
+    "summary": "",
+    "author": "Loym",
+    "data": [
+        "data/ir_actions_server.xml",
+        "data/ir_model_fields.xml",
+        "data/ir_ui_view.xml",
+    ],
+    "depends": [
+        "knowledge",
+    ],
+    "license": "AGPL-3",
+    "version": "18.0.1.0.0",
+    "website": "https://www.loym.com",
+}
