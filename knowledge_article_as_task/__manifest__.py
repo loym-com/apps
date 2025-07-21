4e022df0403da27aafe3ade32279690f291215d1
Henrik Norlin <henrik.norl@gmail.com>
2025-07-21 21:42:53 +0200

    knowledge_article_starred, knowledge_article_as_task: new ids

--- knowledge_article_as_task/__manifest__.py
+++ knowledge_article_as_task/__manifest__.py
@@ -3,10 +3,14 @@
 {
     "name": "Knowledge Article as Task",
     "summary": "",
+    "description": "loymcom/apps-dev 16.0 has functions to deal with properties",
     "author": "Loym",
     "data": [
         "data/ir_actions_server.xml",
+        "data/ir_model_1.xml",
         "data/ir_model_fields.xml",
+        "data/ir_model_2.xml",
+        "data/ir_model_access.xml",
         "data/ir_ui_view.xml",
         "data/ir_actions_act_window.xml",
         "data/ir_ui_menu.xml",
