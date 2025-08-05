2f74d35d3a7544f5a6db30555624f93df3697269
Henrik Norlin <henrik.norl@gmail.com>
2025-08-05 14:11:10 +0200

    [ADD] resource_booking_group

---
+++ resource_booking_group/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2025 Loym - Henrik Norlin
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Resource Booking Group",
+    "summary": "",
+    "author": "Loym, Odoo Community Association (OCA)",
+    "category": "Uncategorized",
+    "data": [
+        "security/ir.model.access.csv",
+        "views/resource_booking_group_views.xml",
+    ],
+    "depends": [
+        "resource_booking",
+        "resource_group",
+        "web_timeline",
+    ],
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "maintainers": ["norlinhenrik"],
+    "version": "16.0.1.0.0",
+    "website": "https://github.com/OCA/",
+}
