6d3133b6b332d2f5c94ae78c3d9b6c69e7bdecce
Henrik Norlin <henrik.norl@gmail.com>
2025-08-05 20:17:48 +0200

    event_sale_resource_booking: create event tickets for product variants

--- event_sale_resource_booking/models/event_event.py
+++ event_sale_resource_booking/models/event_event.py
@@ -1,3 +1,5 @@
+import re
+
 from odoo import api, fields, models
 
 
@@ -8,3 +10,32 @@ class EventEvent(models.Model):
         comodel_name="product.template",
         string="Booking Options",
     )
+
+    @api.constrains("product_tmpl_id")
+    def _create_tickets_for_product_variants(self):
+        Ticket = self.env["event.event.ticket"]
+        if self.product_tmpl_id:
+            self.event_ticket_ids.unlink()
+            for product in self.product_tmpl_id.product_variant_ids:
+                # Search for an event ticket product with the current variant
+                ticket = Ticket.search([
+                    ("event_id", "=", self.id),
+                    ("product_id", "=", product.id),
+                ], limit=1)
+
+                # If no ticket exists for this variant, create one
+                if not ticket:
+                    # name
+                    match = re.search(r"\(([^)]*)\)", product.display_name)
+                    if match:
+                        name = match.group(1)
+                    else:
+                        name = product.display_name
+                    Ticket.create(
+                        {
+                            "name": name,
+                            "event_id": self.id,
+                            "product_id": product.id,
+                            "price": product.lst_price,
+                        }
+                    )
