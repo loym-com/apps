8734788bfe8cd1e98f915c6854afe8b43caa1d4b
Henrik Norlin <henrik.norl@gmail.com>
2025-08-24 18:32:40 +0200

    dev: useful functions, without_demo

---
+++ dev/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "dev",
+    "summary": "",
+    "version": "16.0.1.0.0",
+    "author": "Loym",
+    "website": "",
+    "license": "AGPL-3",
+    "category": "Hidden/Dependency",
+    "depends": [
+        "base",
+    ],
+    "data": [
+    ],
+    "installable": True,
+}
