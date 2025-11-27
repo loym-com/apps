18386ee9663cfa980764fa8da26bd45fd5a8cf31
Henrik Norlin <henrik.norl@gmail.com>
2025-11-27 08:39:06 +0100

    teamm __manifest__.py fix

--- teamm/__manifest__.py
+++ teamm/__manifest__.py
@@ -16,7 +16,6 @@
     "depends": [
         "event_sale_resource_booking",
         "event_sale_resource_booking_timeline",
-        "ref",
         "partner_contact_birthdate",
         "partner_contact_gender",
         "partner_firstname",
