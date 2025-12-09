749b0ef9e457efb3834e223cb92037980e4caff2
Henrik Norlin <henrik.norl@gmail.com>
2025-12-09 12:55:20 +0100

    hr_employee_no_work_contact (renamed hr_employee_email_mobile)

--- hr_employee_no_work_contact/__manifest__.py
+++ hr_employee_no_work_contact/__manifest__.py
@@ -1,8 +1,8 @@
 # Copyright 2025 Henrik Norlin
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "Employee Email & Mobile",
-    "summary": "Not synced with the contact",
+    "name": "Employee Contact Info",
+    "summary": "Do not create work contacts",
     "version": "16.0.1.0.0",
     # "category": "Customer Relationship Management",
     "website": "https://github.com/OCA/partner-contact",
