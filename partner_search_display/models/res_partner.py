e89c18b0e96248f3199559d4bc94a967e185c61e
Henrik Norlin <henrik@loym.com>
2026-02-10 13:19:38 +0100

    partner_search_display: Employee based on work_contact_id

--- partner_search_display/models/res_partner.py
+++ partner_search_display/models/res_partner.py
@@ -10,18 +10,22 @@ class ResPartner(models.Model):
     @api.model
     def name_search(self, name='', args=None, operator='ilike', limit=100):
         result = super().name_search(name=name, args=args, operator=operator, limit=limit)
+
         # Fetch all partner records for the IDs in the result
         partner_ids = [res[0] for res in result]
         partners_sudo = self.browse(partner_ids).sudo() # to read employee_count
 
-        # Modify the text representation based on employee and partner_share fields
+        # Modify the text representation based on hr.employee work_contact_id and partner_share fields
         updated_result = []
-        for p_sudo in partners_sudo:
-            text_repr = result[partner_ids.index(p_sudo.id)][1]
-            if p_sudo.employees_count:
+        employees = self.env['hr.employee'].search([('work_contact_id', 'in', partner_ids)])
+        employee_partner_ids = employees.mapped('work_contact_id.id')
+
+        for partner in partners_sudo:
+            text_repr = result[partner_ids.index(partner.id)][1]
+            if partner.id in employee_partner_ids:
                 text_repr = f"{_('(Employee) ')} {text_repr}"
-            elif not p_sudo.partner_share:
+            elif not partner.partner_share:
                 text_repr = f"{_('(User) ')} {text_repr}"
-            updated_result.append((p_sudo.id, text_repr))
+            updated_result.append((partner.id, text_repr))
 
         return updated_result
