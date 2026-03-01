2a72d0681bdbd677636e1c1070ca9140fdfd0735
Henrik Norlin <henrik@loym.com>
2026-03-01 10:14:57 +0100

    mail_config_by_user: smtp fields are not restricted to system manager

--- mail_config_by_user/models/ir_mail_server.py
+++ mail_config_by_user/models/ir_mail_server.py
@@ -5,3 +5,6 @@ class IrMailServer(models.Model):
     _inherit = 'ir.mail_server'
 
     smtp_user = fields.Char(groups=False)
+    smtp_pass = fields.Char(groups=False)
+    smtp_ssl_certificate = fields.Binary(groups=False)
+    smtp_ssl_private_key = fields.Binary(groups=False)
