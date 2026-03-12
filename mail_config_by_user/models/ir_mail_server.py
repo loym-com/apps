792a1f5e00de1c9b04aa58810e419d3be2df0b3a
Henrik Norlin <henrik@loym.com>
2026-03-12 16:09:42 +0100

    mail_config_by_user: test_smtp_connection with sudo

--- mail_config_by_user/models/ir_mail_server.py
+++ mail_config_by_user/models/ir_mail_server.py
@@ -8,3 +8,16 @@ class IrMailServer(models.Model):
     smtp_pass = fields.Char(groups=False)
     smtp_ssl_certificate = fields.Binary(groups=False)
     smtp_ssl_private_key = fields.Binary(groups=False)
+
+    def test_smtp_connection(self):
+        values = set(
+            self.from_filter,
+            self.smtp_user,
+            self.env.user.login,
+        )
+        if len(values) == 1:
+            # The user is testing its own connection.
+            # Use sudo to allow access to the credentials.
+            return super(IrMailServer, self.sudo()).test_smtp_connection()
+        else:
+            return super().test_smtp_connection()
