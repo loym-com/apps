815ddaf80baa419a1b238d40de5cd48a6eb58f82
Henrik Norlin <henrik@loym.com>
2026-08-20 09:43:06 +0200

    l10n_no_payroll: permisjon uten loennet

--- l10n_no_payroll/models/amelding/amelding_logic.py
+++ l10n_no_payroll/models/amelding/amelding_logic.py
@@ -471,9 +471,6 @@ class AmeldingLogikk:
         p.beskrivelse = _get(
             leave.holiday_status_id, "l10n_no_PermisjonsOgPermitteringsBeskrivelse"
         )
-        mydate = datetime.strptime("2025-08-01", "%Y-%m-%d")
-        if leave.date_from >= mydate:
-            p.loennet = _get(leave.holiday_status_id, "l10n_no_PermisjonLoennet")
         return p
 
     def Permittering(self, leave):
