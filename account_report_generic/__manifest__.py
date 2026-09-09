35db89b76462c16fc0b7d9081a7c11fd4e6a3a3d
Henrik Norlin <henrik@loym.com>
2026-09-09 11:19:39 +0200

    account_report_generic is useful only with One App Free on odoo.com

--- account_report_generic/__manifest__.py
+++ account_report_generic/__manifest__.py
@@ -6,8 +6,10 @@
         'account',
     ],
     'data': [
+        # 'views/account_report_views.xml',
         'data/account_report_data.xml',
     ],
     'license': 'LGPL-3',
     'author': 'Loym',
+    "description": "account.report does not work on Community Edition, since there is no client action / javascript to process account_report. it is only available in the Enterprise Edition.",
 }
